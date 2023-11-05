2039084c58a40adbc72b34244fb69708a30dd69d
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-05 22:44:55 +0100

    22-44

---
+++ src/cat-api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_70dUkozJaKRJH6v6FEFfisLMREnwDOgTiuAgxgnEEsMXPrnFGZVgsw5q9FYgyhoc";
+
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import axios from 'axios';
+
+
+import SlimSelect from 'slim-select';
+
+new SlimSelect({
+  select: '.breed-select'
+});
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+
+
+
+
+
+
+
+
